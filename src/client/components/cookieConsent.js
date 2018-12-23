aaa8722b93499fac7c0282e1ff6e4484878e365e
batuyildiz <batutur@hotmail.com>
2018-12-23 19:36:19 +0300

    fix consent popup destroy

--- src/client/components/cookieConsent.js
+++ src/client/components/cookieConsent.js
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Button from './reusables/button';
 
-const CookieConsent = () => {
-  const hasConsentBefore = !!sessionStorage.getItem('bsContent');
-  if (hasConsentBefore) {
-    return (<div />);
+class CookieConsent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hide: false,
+    };
   }
-  return (
-    <div className="cookie-consent">
-      <div>
-        By using our site, you acknowledge that you have read and understand our
-        <Link to="/cookie"> Cookie Policy</Link>
-        ,
-        <Link to="/privacy"> Privacy Policy</Link>
-        , and our
-        <Link to="/tos"> Terms of Service</Link>
-        .
+  render() {
+    const { hide } = this.state;
+    const hasConsentBefore = !!sessionStorage.getItem('bsContent');
+    if (hasConsentBefore || hide) {
+      return (<div />);
+    }
+    return (
+      <div className="cookie-consent">
+        <div>
+          By using our site, you acknowledge that you have read and understand our
+          <Link to="/cookie"> Cookie Policy</Link>
+          ,
+          <Link to="/privacy"> Privacy Policy</Link>
+          , and our
+          <Link to="/tos"> Terms of Service</Link>
+          .
+        </div>
+        <Button
+          classes={{ button: 'btnClass' }}
+          text="Agree"
+          onClick={() => {
+            this.setState({ hide: true });
+            sessionStorage.setItem('bsContent', true);
+          }}
+          width="100px"
+        />
       </div>
-      <Button
-        classes={{ button: 'btnClass' }}
-        text="Agree"
-        onClick={() => {
-          sessionStorage.setItem('bsContent', true);
-        }}
-        width="100px"
-      />
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default CookieConsent;
