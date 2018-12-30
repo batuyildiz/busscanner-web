af7230c2b5746e3c2ae4ab94ac0e147cebbee2d2
batuyildiz <batutur@hotmail.com>
2018-12-30 18:00:48 +0300

    add contact form

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Button, Collapse } from '@material-ui/core';
 import img from '../assets/images/busscanner-w.png';
 
-const Header = (props) => {
-  const { resetData } = props;
-  return (
-    <Link to="/" onClick={resetData} className="row pt-3">
-      <div className="col-12 col-md-12">
-        <img src={img} alt="logo" width={180} />
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showContact: false,
+    };
+  }
+
+  handleContactClick() {
+    this.setState(state => ({ showContact: !state.showContact }));
+  }
+
+  render() {
+    const { showContact } = this.state;
+    const { resetData } = this.props;
+    return (
+      <div>
+        <div className="header-container">
+          <div className="header-logo">
+            <Link to="/" onClick={resetData}>
+              <img src={img} alt="logo" width={180} />
+            </Link>
+          </div>
+          <div className="header-nav">
+            <Button
+              onClick={() => this.handleContactClick()}
+              style={{ padding: '0 10px' }}
+            >
+              Contact Us
+            </Button>
+          </div>
+        </div>
+        <Collapse in={showContact}>
+          <div className="header-contact">
+            contact@busscanner.net | Pihlaka 97 Ülenurme Tartumaa 61714 | +4915145839541
+          </div>
+        </Collapse>
       </div>
-    </Link>
-  );
-};
+    );
+  }
+}
 
 Header.propTypes = {
   resetData: PropTypes.func.isRequired,
